1a6b2ca8499255f95543ae023b5cb608465891b0
Redmond A. Urbino <rurbino@kaspar>
2014-03-23 11:10:28 -0700

    run TabToSpace on ChangeTabToSpace.cs

--- Assets/Editor/ChangeTabToSpace.cs
+++ Assets/Editor/ChangeTabToSpace.cs
@@ -23,7 +23,7 @@ SOFTWARE.
 
 // git repo at https://github.com/redmondurbino/TabToSpace
 */
-		
+        
 using UnityEngine;
 using System.Collections;
 using UnityEditor;
@@ -35,39 +35,39 @@ using System.Text;
 /// </summary>
 public class ChangeTabToSpace
 {
-	const int spacesPerTab = 4;
-	static string spaces = new string (' ', spacesPerTab);
-	
-	[MenuItem ("Assets/TabToSpace")]
-	/// <summary>
-	/// Converts assets selected in project view from tabs to spaces
-	/// </summary>
-	static void TabToSpace ()
-	{
-		foreach ( Object obj in Selection.objects)
-		{
-			string assetPath = AssetDatabase.GetAssetPath(obj);
-			if (assetPath.Length >0)
-			{
-				// make sure we operate on actual file assets, not on game objects in the scene window
-				string fullPath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + assetPath;
-				Debug.Log (string.Format("Changing tabs to {0} spaces for {1}", spacesPerTab, fullPath));	
-				ConvertOneFile(fullPath);
-			}
-		}
-	}
-	
-	/// <summary>
-	/// Converts the tabs in one file to spaces
-	/// </summary>
-	/// <param name='fullPath'>
-	/// Full path.
-	/// </param>
-	static void ConvertOneFile( string fullPath)
-	{
-		string allText = File.ReadAllText(fullPath);
-		StringBuilder sb = new StringBuilder(allText);
-		sb.Replace("\t", spaces);
-		File.WriteAllText(fullPath, sb.ToString());
-	}
+    const int spacesPerTab = 4;
+    static string spaces = new string (' ', spacesPerTab);
+    
+    [MenuItem ("Assets/TabToSpace")]
+    /// <summary>
+    /// Converts assets selected in project view from tabs to spaces
+    /// </summary>
+    static void TabToSpace ()
+    {
+        foreach ( Object obj in Selection.objects)
+        {
+            string assetPath = AssetDatabase.GetAssetPath(obj);
+            if (assetPath.Length >0)
+            {
+                // make sure we operate on actual file assets, not on game objects in the scene window
+                string fullPath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + assetPath;
+                Debug.Log (string.Format("Changing tabs to {0} spaces for {1}", spacesPerTab, fullPath));    
+                ConvertOneFile(fullPath);
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Converts the tabs in one file to spaces
+    /// </summary>
+    /// <param name='fullPath'>
+    /// Full path.
+    /// </param>
+    static void ConvertOneFile( string fullPath)
+    {
+        string allText = File.ReadAllText(fullPath);
+        StringBuilder sb = new StringBuilder(allText);
+        sb.Replace("\t", spaces);
+        File.WriteAllText(fullPath, sb.ToString());
+    }
 }
