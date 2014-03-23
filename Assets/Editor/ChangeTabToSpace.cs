25c737dfd888ed531a86a80bac653933296116cd
Redmond A. Urbino <rurbino@kaspar>
2014-03-23 10:49:02 -0700

    tab to space works

--- Assets/Editor/ChangeTabToSpace.cs
+++ Assets/Editor/ChangeTabToSpace.cs
@@ -1,15 +1,48 @@
 using UnityEngine;
 using System.Collections;
+using UnityEditor;
+using System.IO;
+using System.Text;
 
-public class ChangeTabToSpace : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+/// <summary>
+/// Classe for changing multiple selected files in Project view from tabs to spaces 
+/// </summary>
+public class ChangeTabToSpace
+{
+	const int spacesPerTab = 4;
+	static string spaces = new string (' ', spacesPerTab);
 	
+	[MenuItem ("Assets/TabToSpace")]
+	/// <summary>
+	/// Converts assets selected in project view from tabs to spaces
+	/// </summary>
+	static void TabToSpace ()
+	{
+		foreach ( Object obj in Selection.objects)
+		{
+			//UnityEngine.Object  obj = objs[i] as UnityEngine.Object;
+			string assetPath = AssetDatabase.GetAssetPath(obj);
+			if (assetPath.Length >0)
+			{
+				// make sure we operate on actual file assets, not on game objects in the scene window
+				string fullPath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + assetPath;
+				Debug.Log (string.Format("Changing tabs to {0} spaces for {1}", spacesPerTab, fullPath));	
+				ConvertOneFile(fullPath);
+			}
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	/// <summary>
+	/// Converts the tabs in one file to spaces
+	/// </summary>
+	/// <param name='fullPath'>
+	/// Full path.
+	/// </param>
+	static void ConvertOneFile( string fullPath)
+	{
+		string allText = File.ReadAllText(fullPath);
+		StringBuilder sb = new StringBuilder(allText);
+		sb.Replace("\t", spaces);
+		File.WriteAllText(fullPath, sb.ToString());
 	}
 }
